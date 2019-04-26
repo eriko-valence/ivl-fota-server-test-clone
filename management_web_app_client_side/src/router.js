7953b46444ed3a3509ddce5061fbaa62a4842ab4
Erik Olsen <eriko@valencelevel.com>
2019-04-25 17:30:38 -0700

    Add global route guard

--- management_web_app_client_side/src/router.js
+++ management_web_app_client_side/src/router.js
@@ -4,10 +4,12 @@ import Home from './components/Home.vue'
 import FirmwareManagement from './views/FirmwareManagement.vue'
 import GroupManagement from './views/GroupManagement.vue'
 import DeviceManagement from './views/DeviceManagement.vue'
+import authentication from './authentication'
 
 Vue.use(Router)
 
-export default new Router({
+const router = new Router({
+  mode: 'history',
   routes: [
     {
       path: '/home',
@@ -27,7 +29,31 @@ export default new Router({
     {
       path: '/devicemanagement',
       name: 'DeviceManagement',
-      component: DeviceManagement
+      component: DeviceManagement,
+      meta: {
+        requiresAuthentication: true
+      }
     }
   ]
 })
+
+// Global route guard
+router.beforeEach((to, from, next) => {
+  console.log('beforeEach');
+  console.log(to);
+  if (to.matched.some(record => record.meta.requiresAuthentication)) {
+    console.log('matched');
+    // this route requires auth, check if logged in
+    if (authentication.isAuthenticated()) {
+      // only proceed if authenticated.
+      console.log('authenticated!')
+      next();
+    } else {
+      authentication.signIn();
+    }
+  } else {
+    next();
+  }
+});
+
+export default router;
