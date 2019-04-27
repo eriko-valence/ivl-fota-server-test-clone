d9f1a53e238139e77847bfce2a96284f7c440ce8
Erik Olsen <eriko@valencelevel.com>
2019-04-26 21:47:11 -0700

    Add azure sql scaffolding

--- device_api/CheckFirmware/index.js
+++ device_api/CheckFirmware/index.js
@@ -1,16 +1,60 @@
-module.exports = async function (context, req) {
-    context.log('JavaScript HTTP trigger function processed a request.');
+var Connection = require('tedious').Connection;
+var Request = require('tedious').Request;
 
-    if (req.query.name || (req.body && req.body.name)) {
-        context.res = {
-            // status: 200, /* Defaults to 200 */
-            body: "Hello " + (req.query.name || req.body.name)
-        };
+module.exports = function (context, req) {
+
+    var config =
+    {
+        authentication: {
+            options: {
+                userName: process.env.AzureSqlServerLoginName,
+                password: process.env.AzureSqlServerLoginPass
+            },
+            type: 'default'
+        },
+        server: process.env.AzureSqlServerName,
+        options:
+        {
+            database: process.env.AzureSqlDatabaseName,
+            encrypt: true
+        }
     }
-    else {
-        context.res = {
-            status: 400,
-            body: "Please pass a name on the query string or in the request body"
-        };
+
+    var connection = new Connection(config);
+    connection.on('connect', function(err) {
+        getFirmwareManifest();
+    });
+
+    function getFirmwareManifest() {
+
+        request = new Request("SELECT Version, Signature, RTRIM(Uri) as Uri, Md5 from FirmwareManifest", function(err) {
+        if (err) {
+            console.log(err);}
+        });
+
+        let firmware = {};
+
+        //TODO - Handle more than one row in the result set
+        request.on('row', function(columns) {
+            columns.forEach(function(column) {
+                firmware[column.metadata.colName] = column.value;
+            });
+            context.res = {
+                status: 200,
+                body: firmware
+            };
+            context.done();
+            
+        });
+
+        //request.on('requestCompleted', function () {
+        //    cleanUp();
+        //});
+        connection.execSql(request);
     }
-};
+
+    function cleanUp() {
+        //context.done();
+    }
+
+}
