cd982d01e285a020790316b55610191c3f6e6758
Erik Olsen <eriko@valencelevel.com>
2019-04-29 13:31:11 -0700

    Add azure blob sas generation + fwcheck logic updates

--- device_api/CheckFirmware/index.js
+++ device_api/CheckFirmware/index.js
@@ -1,5 +1,6 @@
 var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
+var azure = require('azure-storage');
 
 module.exports = function (context, req) {
 
@@ -27,34 +28,71 @@ module.exports = function (context, req) {
 
     function getFirmwareManifest() {
 
-        request = new Request("SELECT Version, Signature, RTRIM(Uri) as Uri, Md5 from FirmwareManifest", function(err) {
+        let deviceid = req.params['deviceid'];
+        let reportedVersion = req.query['ver'];
+        let sqlQuery = `SELECT * from vwCheckFirmware where DeviceId = '${deviceid}' AND DesiredVersion > '${reportedVersion}'`;
+
+        request = new Request(sqlQuery, function(err) {
         if (err) {
             console.log(err);}
         });
 
-        let firmware = {};
+        let desiredFirmware = {};
 
-        //TODO - Handle more than one row in the result set
         request.on('row', function(columns) {
             columns.forEach(function(column) {
-                firmware[column.metadata.colName] = column.value;
+                desiredFirmware[column.metadata.colName] = column.value;
             });
-            context.res = {
-                status: 200,
-                body: firmware
-            };
-            context.done();
-            
         });
 
-        //request.on('requestCompleted', function () {
-        //    cleanUp();
-        //});
+        request.on('requestCompleted', function () {
+            let sasToken = generateSasToken(desiredFirmware['BlobContainer'], desiredFirmware['BlobName'], null);
+            context.res = {
+                status: 200,
+                body: {
+                    version: desiredFirmware['DesiredVersion'],
+                    signature: desiredFirmware['Signature'],
+                    uri: sasToken.uri,
+                    md5: desiredFirmware['Md5']
+                }
+            };
+            context.done();
+
+        });
         connection.execSql(request);
     }
 
-    function cleanUp() {
-        //context.done();
-    }
+    /*
+      Returns a SAS token for Azure Storage for the specified container. 
+      You can also optionally specify a particular blob name and access permissions. 
+      To learn more, see https://github.com/Azure-Samples/functions-dotnet-sas-token/blob/master/README.md
+    */
+   function generateSasToken(container, blobName, permissions) {
+    var connString = process.env.AzureWebJobsStorage;
+    var blobService = azure.createBlobService(connString);
+
+    // Create a SAS token that expires in an hour
+    // Set start time to five minutes ago to avoid clock skew.
+    var startDate = new Date();
+    startDate.setMinutes(startDate.getMinutes() - 5);
+    var expiryDate = new Date(startDate);
+    expiryDate.setMinutes(startDate.getMinutes() + 60);
+
+    permissions = permissions || azure.BlobUtilities.SharedAccessPermissions.READ;
+
+    var sharedAccessPolicy = {
+        AccessPolicy: {
+            Permissions: permissions,
+            Start: startDate,
+            Expiry: expiryDate
+        }
+    };
+    var sasToken = blobService.generateSharedAccessSignature(container, blobName, sharedAccessPolicy);
+    
+    return {
+        token: sasToken,
+        uri: blobService.getUrl(container, blobName, sasToken, true)
+    };
+}
 
 }
