5fd83f861e9268e992098224bde4b192f4d3e2e9
Erik Olsen <eriko@valencelevel.com>
2019-04-30 17:36:04 -0700

    Add azure key vault for storing azure sql creds

--- device_api/CheckFirmware/index.js
+++ device_api/CheckFirmware/index.js
@@ -2,38 +2,63 @@ var Connection = require('tedious').Connection;
 var Request = require('tedious').Request;
 var azure = require('azure-storage');
 var _ = require('lodash');
+const KeyVault = require('azure-keyvault');
+const msRestAzure = require('ms-rest-azure');
 
 module.exports = function (context, req) {
 
-    //azure sql database connection configuration
-    var config =
-    {
-        authentication: {
-            options: {
-                userName: process.env.AzureSqlServerLoginName,
-                password: process.env.AzureSqlServerLoginPass
-            },
-            type: 'default'
-        },
-        server: process.env.AzureSqlServerName,
-        options:
-        {
-            database: process.env.AzureSqlDatabaseName,
-            encrypt: true
-        }
-    }
+    let secret = process.env.AzureADClientSecret;
+    let clientId = process.env.AzureADClientID;
+    let domain = process.env.AzureADTenantID;
 
-    //initiate sql database connection
-    var connection = new Connection(config);
-    connection.on('connect', function(err) {
-        getFirmwareManifest();
+    // The ms-rest-azure library allows us to login with MSI by providing the resource name. In this case the resource is Key Vault.
+    // For public regions the resource name is Key Vault
+    //msRestAzure.loginWithAppServiceMSI({resource: 'https://vault.azure.net'}).then( (credentials) => {
+    msRestAzure.loginWithServicePrincipalSecret(clientId, secret, domain).then((credentials) => {
+        const keyVaultClient = new KeyVault.KeyVaultClient(credentials);
+
+        var keyVaultname = process.env.AzureKeyVaultName;
+        var vaultUri = "https://" + keyVaultname + ".vault.azure.net/";
+
+        let var1 = keyVaultClient.getSecret(vaultUri, "AzureSqlServerLoginName", "");
+        let var2 = keyVaultClient.getSecret(vaultUri, "AzureSqlServerLoginPass", "");
+
+        Promise.all([var1, var2]).then(function(results) {
+            //azure sql creds pulled from azure key vault
+            let azureSqlLoginName = _.get(results[0], 'value', '');
+            let azureSqlLoginPass = _.get(results[1], 'value', '');
+
+            //azure sql database connection configuration
+            var config =
+            {
+                authentication: {
+                    options: {
+                        userName: azureSqlLoginName,
+                        password: azureSqlLoginPass
+                    },
+                    type: 'default'
+                },
+                server: process.env.AzureSqlServerName,
+                options:
+                {
+                    database: process.env.AzureSqlDatabaseName,
+                    encrypt: true
+                }
+            };
+                    //initiate sql database connection
+                    var connection = new Connection(config);
+                    connection.on('connect', function(err) {
+                        getFirmwareManifest(connection);
+                    });
+            });
+
     });
 
     /*
       Returns the manifest for a version of firmware that the server determines that the fridge 
       should download and apply.
     */
-    function getFirmwareManifest() {
+    function getFirmwareManifest(connection) {
 
         //let deviceid = req.params['deviceid']; //pull deviceid from route parameter
         let deviceid = _.get(req.params, 'deviceid', ''); //pull deviceid from route parameter
@@ -44,8 +69,9 @@ module.exports = function (context, req) {
 
         //represents an azure sql query request that can be executed on a connection
         request = new Request(sqlQuery, function(err) {
-        if (err) {
-            console.log(err);}
+            if (err) {
+                console.log(err);
+            }
         });
 
         let desiredFirmware = [];
@@ -99,31 +125,31 @@ module.exports = function (context, req) {
       To learn more, see https://github.com/Azure-Samples/functions-dotnet-sas-token/blob/master/README.md
     */
    function generateSasToken(container, blobName, permissions) {
-    var connString = process.env.AzureWebJobsStorage;
-    var blobService = azure.createBlobService(connString);
+        var connString = process.env.AzureWebJobsStorage;
+        var blobService = azure.createBlobService(connString);
 
-    // Create a SAS token that expires in an hour
-    // Set start time to five minutes ago to avoid clock skew.
-    var startDate = new Date();
-    startDate.setMinutes(startDate.getMinutes() - 5);
-    var expiryDate = new Date(startDate);
-    expiryDate.setMinutes(startDate.getMinutes() + 60);
+        // Create a SAS token that expires in an hour
+        // Set start time to five minutes ago to avoid clock skew.
+        var startDate = new Date();
+        startDate.setMinutes(startDate.getMinutes() - 5);
+        var expiryDate = new Date(startDate);
+        expiryDate.setMinutes(startDate.getMinutes() + 60);
 
-    permissions = permissions || azure.BlobUtilities.SharedAccessPermissions.READ;
+        permissions = permissions || azure.BlobUtilities.SharedAccessPermissions.READ;
 
-    var sharedAccessPolicy = {
-        AccessPolicy: {
-            Permissions: permissions,
-            Start: startDate,
-            Expiry: expiryDate
-        }
-    };
-    var sasToken = blobService.generateSharedAccessSignature(container, blobName, sharedAccessPolicy);
-    
-    return {
-        token: sasToken,
-        uri: blobService.getUrl(container, blobName, sasToken, true)
-    };
-}
+        var sharedAccessPolicy = {
+            AccessPolicy: {
+                Permissions: permissions,
+                Start: startDate,
+                Expiry: expiryDate
+            }
+        };
+        var sasToken = blobService.generateSharedAccessSignature(container, blobName, sharedAccessPolicy);
+        
+        return {
+            token: sasToken,
+            uri: blobService.getUrl(container, blobName, sasToken, true)
+        };
+    }
 
 }
