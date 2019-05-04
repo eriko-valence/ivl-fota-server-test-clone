532d0e8c6ab69da53e529a4a62c482eac95f1bf8
Erik Olsen <eriko@valencelevel.com>
2019-05-03 17:10:08 -0700

    Add management api - delete group

---
+++ admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspDeleteGroup.sql
@@ -0,0 +1,33 @@
+﻿/*
+
+Deletes existing group from MFOX DB.  The following requirements apply to group delete:
+
+Only groups not currently assigned to any devices can be deleted.
+Restrictions around deleting a group will be handled at the DB level using stored procedures.
+
+*/
+
+CREATE PROCEDURE [dbo].[uspDeleteGroup]
+  @groupid int, @result INT OUTPUT
+AS
+BEGIN
+ 
+ DECLARE @devicecount INT, @groupexists INT
+
+SET @devicecount = (select count(*) from Device where DeviceGroupId = @groupid);
+
+SET @groupexists = (select count(*) from DeviceGroup where Id = @groupid);
+
+IF @groupexists = 0
+    SET @result = 2; --Group does not exist
+ELSE IF @devicecount > 0
+	SET @result = 3; --Only groups not currently assigned to any devices can be deleted.
+ELSE
+	BEGIN
+	delete from DeviceGroup where Id = @groupid;
+		if @@rowcount = 1
+		  SET @result = 1 --successful deletion
+		else
+		  SET @result = 4 --group not deleted 
+	END
+END
