2d003376db954cd75c3d7217fa7dd6b2ed42f625
Erik Olsen <eriko@valencelevel.com>
2019-05-06 16:18:36 -0700

    Add device group foreign key

--- admin_sql_db/admin_sql_db/dbo/Tables/DeviceGroup.sql
+++ admin_sql_db/admin_sql_db/dbo/Tables/DeviceGroup.sql
@@ -2,6 +2,9 @@
     [Id]                INT         IDENTITY (1, 1) NOT NULL,
     [Name]              NCHAR (100) NULL,
     [DesiredFirmwareId] INT         NULL,
-    PRIMARY KEY CLUSTERED ([Id] ASC)
+    PRIMARY KEY CLUSTERED ([Id] ASC),
+    FOREIGN KEY ([DesiredFirmwareId]) REFERENCES [dbo].[DeviceFirmware] ([Id])
 );
+
+
 
