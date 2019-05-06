110fd9d369aeee0d7f7a11e5897a3ded562eef4a
Erik Olsen <eriko@valencelevel.com>
2019-05-06 10:20:24 -0700

    Add management api - get all firmware

---
+++ admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspGetAllFirmware.sql
@@ -0,0 +1,5 @@
+﻿CREATE PROCEDURE [dbo].[uspGetAllFirmware]
+AS
+BEGIN
+  select * from vwFirmware
+END
---
+++ admin_sql_db/admin_sql_db/dbo/Views/vwFirmware.sql
@@ -0,0 +1,11 @@
+﻿
+CREATE VIEW [vwFirmware] AS
+SELECT 
+  RTRIM(t1.Id) as 'FirmwareId', 
+  RTRIM(t1.Version) as 'Version', 
+  RTRIM(t1.Signature) as 'Signature', 
+  RTRIM(t1.Md5) as 'Md5',
+  RTRIM(t1.BlobName) as 'BlobName', 
+  RTRIM(t1.BlobContainer) as 'BlobContainer'
+FROM
+  DeviceFirmware t1
