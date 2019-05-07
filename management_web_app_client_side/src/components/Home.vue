ba5b2e7c93a6fcafbcdcd91cc11073eb834da11f
Erik Olsen <eriko@valencelevel.com>
2019-05-06 21:47:20 -0700

    Fix bug - vue components initialized three times

--- management_web_app_client_side/src/components/Home.vue
+++ management_web_app_client_side/src/components/Home.vue
@@ -2,23 +2,21 @@
   <div class="home">
   <b-card no-body>
   <b-tabs small card vertical>
+    <router-view></router-view>
     <b-tab content-class="mt-3" active>
       <template slot="title">
         <b-link :to="{ path: '/firmware'}" replace>Firmware Management</b-link>
       </template>
-      <router-view></router-view>
     </b-tab>
     <b-tab>
       <template slot="title">
         <b-link :to="{ path: '/groupmanagement'}" replace>Group Management</b-link>
       </template>
-      <router-view></router-view>
     </b-tab>
     <b-tab>
       <template slot="title">
         <b-link :to="{ path: '/devicemanagement'}" replace>Device Management</b-link>
       </template>
-      <router-view></router-view>
     </b-tab>
   </b-tabs>
   </b-card>
