b29d6949983b4336f3f16f5904dc0af2adf78302
Erik Olsen <eriko@valencelevel.com>
2019-05-08 17:21:27 -0700

    Update firmware management page - integrate azure storage sas token

--- admin_api/UploadUri/index.js
+++ admin_api/UploadUri/index.js
@@ -3,14 +3,15 @@ var _ = require('lodash');
 
 module.exports = function (context, req) {
 
-    let name = _.get(req.query, 'name', ''); //pull deviceid from route parameter
-    console.log('name: ' + name);
-    console.log(req);
+    let name = _.get(req.query, 'name', null); //pull deviceid from route parameter
+    if (name !== null) {
+        name = `${process.env.AzureBlobNamePrefix}${name}`;
+    }
 
     // The following values can be used for permissions: 
     // "a" (Add), "r" (Read), "w" (Write), "d" (Delete), "l" (List)
     // Concatenate multiple permissions, such as "rwa" = Read, Write, Add
-    let sasToken = helper.generateSasToken("fota", name, "rwa");
+    let sasToken = helper.generateSasToken(process.env.AzureBlobContainer, name, "racwl");
 
     let bodySasUri = {sas_uri: sasToken.uri};
 
--- management_web_app_client_side/src/views/FirmwareManagement.vue
+++ management_web_app_client_side/src/views/FirmwareManagement.vue
@@ -40,6 +40,7 @@ import authentication from '../authentication'
               this.firmwareVersion = '';
               this.firmwareSignature = '';
               this.firmwareImage = '';
+              this.firmwareFileContent = '';
           },
           handleOk(evt) {
               // Prevent modal from closing
@@ -52,8 +53,6 @@ import authentication from '../authentication'
               }
           },
           handleSubmit() {
-              console.log('handleSubmitModal');
-              console.log(this.firmware);
               this.firmware.push(this.firmwareVersion)
 
                 this.firmwareUploadBodyMFOX = {
@@ -61,9 +60,6 @@ import authentication from '../authentication'
                   signature : this.firmwareSignature,
                   image    : this.firmwareImage
                 };
-
-                console.log(this.firmwareUploadBodyMFOX);
-
                 this.getAzureStorageSasToken(this.firmwareImage);
 
               this.handleLoadModal()
@@ -72,58 +68,65 @@ import authentication from '../authentication'
               })
          },
          async loadTextFromFile(ev) {
-
-
-                console.log('loadTextFromFile');
+                this.firmwareFileContent = ev.target.files[0];
                 const file = ev.target.files[0];
                 this.firmwareImage = file.name;
-                const reader = new FileReader();
-                console.log(file);
+                console.log(`file.name: ${file.name}`);
+                console.log(`firmwareImage: ${this.firmwareImage}`);
 
-                //const anonymousCredential = new AnonymousCredential();
-                // Use TokenCredential with OAuth token
-                //const tokenCredential = new TokenCredential("token");
-                //tokenCredential.token = "renewedToken"; // Renew the token by updating token field of token credential
-                reader.onload = e => this.$emit("load", e.target.result);
-                reader.readAsText(file);
+                var reader = new FileReader();
+                reader.onload = function(){
+                  //this.firmwareFileContent = reader.result;
+                  //var output = document.getElementById('output');
+                  //output.src = dataURL;
+                  //console.log(this.firmwareFileContent);
+                  //console.log(output);
+                };
+                //reader.readAsDataURL(input.files[0]);
+                //const reader = new FileReader();
+                //reader.onload = e => this.$emit("load", e.target.result);
+                //reader.onload = async e => {
 
+                //}
+                //reader.readAsText(file);
+                reader.readAsBinaryString(file);
         },
         getAzureStorageSasToken(blobName) {
-
             let apiEndpoint = `https://ivlapiadmin.azurewebsites.net/v1/upload_uri?name=${blobName}`;
             let accessToken = `Bearer ${authentication.getAccessToken()}`;
-                                console.log('--this');
-                    console.log(this);
-                    console.log('--this');
+            console.log(`Step #1a: Acquire azure storage sas token for blob ${blobName}....`);
+            console.log(`Step #1b: Api endpoint: ${apiEndpoint}`);
+            console.log(`Step #1c: this.firmwareFileContent: ${this.firmwareFileContent}`);
+            var fileContent = this.firmwareFileContent;
+            var blobName = this.firmwareImage;
             this.axios.get(apiEndpoint, {headers: {'authorization': accessToken}})
                     .then((response) => {
-                    console.log('success');
-                    console.log(response.data);
-                    console.log('----self');
-                    //console.log(this);
-                    console.log(this.uploadFirmwareToAzureBlob(response.data.sas_uri));
+                    console.log(`Step #1d: this.firmwareFileContent: ${fileContent}`);
+                    console.log(`Step #1e: Successfully acquired sas token: ${response.data.sas_uri}`);
+                    console.log(this.uploadFirmwareToAzureBlob(response.data.sas_uri, fileContent, blobName));
                     console.log(this.uploadToMFOX());
-                    console.log('----self');
                     }).catch(function (error) {
                     console.log('error');
                     console.log(error);
             });
-
         },
         uploadToMFOX() {
+            console.log(`Step #3a: Upload firmware metadata to MFOX...`);
             let apiEndpoint = 'https://ivlapiadmin.azurewebsites.net/api/Firmware';
             let accessToken = `Bearer ${authentication.getAccessToken()}`;
             this.axios.post(apiEndpoint, this.firmwareUploadBodyMFOX, {headers: {'authorization': accessToken}})
                     .then(function (response) {
                     console.log('success');
+                    console.log(`Step #3b: Successfully uploaded firmware metadata to MFOX...`);
                     console.log(response.data);
                     }).catch(function (error) {
                     console.log('error');
                     console.log(error);
             });
         },
-        async uploadFirmwareToAzureBlob(uploadUri) {
-                console.log('uploadUri: ' + uploadUri);
+        async uploadFirmwareToAzureBlob(uploadUri, fileContent, blobName) {
+          console.log(`Step #2a: Upload firmware to azure storage blob container...`);
+          console.log(this);
 
                 const {
                 AnonymousCredential,
@@ -135,13 +138,7 @@ import authentication from '../authentication'
                 Aborter
                 } = require("@azure/storage-blob");
 
-                console.log('GET to retrieve firmware upload URI');
-
-                let upload_uri = 'https://ivlapiadmin.azurewebsites.net/v1/upload_uri';
-
-                console.log('POST to write new firmware information to MFOX');
-
-
+                //let upload_uri = `https://ivlapiadmin.azurewebsites.net/v1/upload_uri?name=${blobName}`;
 
                 // Enter your storage account name and shared key
                 const account = "ivlapidevice916d";
@@ -152,30 +149,65 @@ import authentication from '../authentication'
                 const anonymousCredential = new AnonymousCredential();
                 const pipeline = StorageURL.newPipeline(anonymousCredential);
 
-                let blobUrlSas = 'https://ivlapidevice916d.blob.core.windows.net/fota?st=2019-05-07T17%3A09%3A13Z&se=2019-05-08T17%3A09%3A13Z&sp=racwl&sv=2018-03-28&sr=c&sig=G8hvIiYFJmEVLJUGHSv45FAjoM71FrvTyWJkLsz9LpQ%3D';
+                //let blobUrlSas = 'https://ivlapidevice916d.blob.core.windows.net/fota?st=2019-05-07T17%3A09%3A13Z&se=2019-05-08T17%3A09%3A13Z&sp=racwl&sv=2018-03-28&sr=c&sig=G8hvIiYFJmEVLJUGHSv45FAjoM71FrvTyWJkLsz9LpQ%3D';
+                //uploadUri = 'https://ivlapidevice916d.blob.core.windows.net/fota?st=2019-05-08T17%3A23%3A56Z&se=2019-05-09T17%3A23%3A56Z&sp=racwl&sv=2018-03-28&sr=c&sig=%2FYqYsF%2BiWm18xHGMJNFksGqNwM8Qm4Gigcs3FQ%2FQlq4%3D';
+                //uploadUri =        'https://ivlapidevice916d.blob.core.windows.net/fota?st=2019-05-08T20%3A55%3A11Z&se=2019-05-08T21%3A55%3A11Z&sp=racwl&sv=2018-03-28&sr=c&sig=Jog7XGBVnCE%2FLh1%2Bj3BwVZKxHqsQ3a7tAMxVTiD%2BEBM%3D';
+                //uploadUri = 'https://ivlapidevice916d.blob.core.windows.net/fota/fota/01.json?st=2019-05-08T22%3A56%3A37Z&se=2019-05-09T22%3A56%3A37Z&sp=racwl&sv=2018-03-28&sr=b&sig=pBCqqYYj%2BvuDiGoPzHTqqoIpNSg3ah2qRdDgRzdzcqs%3D';
 
                 const serviceURL = new ServiceURL(
                     // When using AnonymousCredential, following url should include a valid SAS or support public access
-                    blobUrlSas,
+                    uploadUri,
                     pipeline
                 );
 
+                //const reader = new FileReader();
+                //reader.onload = e => this.$emit("load", e.target.result);
+                //console.log(`this.firmwareImage: ${this.firmwareImage}`);
+                //reader.onload = async e => {
+                  //var text = reader.result;
+
+                  let containerName = "fota";
+                  let url = 'https://ivlapidevice916d.blob.core.windows.net';
+                  const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
+                  const content = fileContent;
+                  //const blobName = blobImageName;
+                  const blobURL = BlobURL.fromContainerURL(containerURL, blobName);
+                  const blockBlobURL = BlockBlobURL.fromBlobURL(serviceURL);
+                  console.log('------------------------------------');
+                  console.log(`uploadUri: ${uploadUri}`);
+                  console.log(`serviceURL:  ${serviceURL}`);
+                                    console.log(`serviceURL:  ${serviceURL.url}`);
+                                    console.log(serviceURL);
+                  console.log(`blobName: ${blobName}`)
+                  console.log(`containerName: ${containerName}`);
+                  console.log(`content: ${content}`);
+                  console.log(content);
+                  console.log(`containerURL: ${containerURL.url}`);
+                  console.log(`blobURL: ${blobURL.url}`);
+                  console.log(blobURL);
+                  console.log(`blockBlobURL: ${blockBlobURL.url}`);
+                  console.log(blockBlobURL);
+                  console.log('------------------------------------');
+                  const uploadBlobResponse = await blockBlobURL.upload(
+                    //const uploadBlobResponse = await serviceURL.upload(
+                      Aborter.none,
+                      content,
+                      content.size
+                );
+                console.log(`Step #2b: Succssfully uploaded firmware to azure storage blob container... ${uploadBlobResponse.requestId}`);
+
+                //}
+                //console.log('----------------------------');
+                //console.log(this.firmwareFileContent);
+                //console.log('----------------------------');
+                //reader.readAsText(this.firmwareFileContent);
+
                 // Create a blob
-                let containerName = "fota";
-                const containerURL = ContainerURL.fromServiceURL(serviceURL, containerName);
-                const content = "hello";
-                const blobName = "newblob" + new Date().getTime();
-                const blobURL = BlobURL.fromContainerURL(containerURL, blobName);
-                const blockBlobURL = BlockBlobURL.fromBlobURL(blobURL);
-                const uploadBlobResponse = await blockBlobURL.upload(
-                    Aborter.none,
-                    content,
-                    content.length
-                );
-                console.log(
-                    `Upload block blob ${blobName} successfully`,
-                    uploadBlobResponse.requestId
-                );
+
+                //console.log(
+                //    `Upload block blob ${blobName} successfully`,
+                //    uploadBlobResponse.requestId
+                //);
 
         }
     },
