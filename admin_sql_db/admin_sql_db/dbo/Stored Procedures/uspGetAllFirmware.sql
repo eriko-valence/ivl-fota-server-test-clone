0a147bb6bbf6f820d26003e321489a8aeed966ea
Erik Olsen <eriko@valencelevel.com>
2019-05-13 08:53:30 -0700

    Add temp workaround for displaying long firmware signature

--- admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspGetAllFirmware.sql
+++ admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspGetAllFirmware.sql
@@ -1,5 +1,8 @@
 ﻿CREATE PROCEDURE [dbo].[uspGetAllFirmware]
 AS
 BEGIN
-  select * from vwFirmware
+  --select * from vwFirmware
+  --TODO: Substring just for temp dev purposes - replace substring with a 'copy link' feature in web app
+  select Id as 'FirmwareId', Version, (SUBSTRING(Signature, 0, 25) + '...') as Signature, Md5, BlobName, BlobContainer from DeviceFirmware
+
 END
