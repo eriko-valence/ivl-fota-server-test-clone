dc630b5786fc364b78794117dba3ebb2beb29723
Erik Olsen <eriko@valencelevel.com>
2019-05-14 16:41:01 -0700

    Update device management page - refactor device display

--- management_web_app_client_side/src/views/DeviceManagement.vue
+++ management_web_app_client_side/src/views/DeviceManagement.vue
@@ -1,158 +1,117 @@
 <template>
-
-<div>
-
-  <b-table  striped hover :items="allDevices" :fields="fields"></b-table>
-
-<b-button v-b-modal.modal-prevent>Create New Device</b-button>
-    <b-modal
-      id="modal-prevent"
-      ref="modal"
-      title="Upload File"
-      @ok="handleOk"
-      @shown="handleLoadModal"
-    >
+  <div>
+    <b-table  striped hover :items="allDevices" :fields="fields"></b-table>
+    <b-button v-b-modal.modal-prevent>Create New Device</b-button>
+    <b-modal id="modal-prevent" ref="modal" title="Upload File" @ok="handleOk" @shown="handleLoadModal">
       <form @submit.stop.prevent="handleSubmit">
         <b-form-input v-model="inputDeviceId" placeholder="Enter device id"></b-form-input>
-
-<b-form-select v-model="selectedGroup" :options="ddGroups" class="mb-3">
-      <template slot="first">
-        <option :value="null" disabled>-- Please select a group --</option>
-      </template>
-    </b-form-select>
-
-    <span>Selected: {{ selectedGroup }}</span>
-
+        <b-form-select v-model="selectedGroup" :options="ddGroups" class="mb-3">
+        <template slot="first">
+          <option :value="null" disabled>-- Please select a group --</option>
+        </template>
+      </b-form-select>
       </form>
     </b-modal>
-</div>
+  </div>
 </template>
 
 <script>
-
 import authentication from '../authentication'
- 
-  //import axios from 'axios'
-  export default {
-    /* define data properties */
-      data() {
-          return {
-              inputDeviceId: '',
-              firmware: [],
-              allFirmware: [],
-              deviceUploadBodyMFOX: {},
-              allDevices: [],
-              selectedGroup: '',
-              ddGroups: [],
-              fields: [
-                  { key: 'deviceid', label: 'Device ID', sortable: true, sortDirection: 'desc' },
-                  { key: 'group', label: 'Group', sortable: true, class: 'text-center' },
-                  { key: 'desired_fw', label: 'Desired FW Version', sortable: true, class: 'text-center' },
-                  { key: 'reported_fw', label: 'Reported FW Version', sortable: true, class: 'text-center' },
-                  { key: 'last_report_date', label: 'Last Report Date', sortable: true, class: 'text-center' }
-              ],
-           }
-      },
-      methods: {
-          handleLoadModal() {
-              console.log('handleLoadModal');
-              this.inputDeviceId = '';
-              this.selectedGroup = '';
-          },
-          handleOk(evt) {
-              // Prevent modal from closing
-              console.log('handleOk');
-              evt.preventDefault()
-              if (!this.inputDeviceId) {
-                  alert('Please enter a device id')
-              } else if (!this.selectedGroup) {
-                  alert('Please select a group')
-              } else {
-                 this.handleSubmit()
-              }
-          },
-          handleSubmit() {
-                this.deviceUploadBodyMFOX = {
-                  deviceid: this.inputDeviceId,
-                  group_id : this.selectedGroup
-                };
-              this.handleLoadModal()
-              this.$nextTick(() => {
-                this.$refs.modal.hide()
-              })
-              this.uploadToMFOX();
-         },
-         async loadTextFromFile(ev) {
-                this.firmwareFileContent = ev.target.files[0];
-                const file = ev.target.files[0];
-                this.firmwareImage = file.name;
-                console.log(`file.name: ${file.name}`);
-                console.log(`firmwareImage: ${this.firmwareImage}`);
-                var reader = new FileReader();
-                reader.onload = function(){
-                };
-                reader.readAsBinaryString(file);
-        },
-        uploadToMFOX() {
-            console.log(`Step #3a: Upload group to MFOX...`);
-            let apiEndpoint = 'https://ivlapiadmin.azurewebsites.net/v1/devices';
-            let accessToken = `Bearer ${authentication.getAccessToken()}`;
-            console.log('################################################');
-            console.log(this.deviceUploadBodyMFOX);
-            console.log('################################################');
-            this.axios.post(apiEndpoint, this.deviceUploadBodyMFOX, {headers: {'authorization': accessToken}})
-                    .then( (response) => {
-                    console.log('success');
-                    console.log(`Step #3b: Successfully uploaded group to MFOX...`);
-                    this.getAllDevices();
-                    console.log(response.data);
-                    }).catch( (error) => {
-                    console.log('error');
-                    console.log(error);
-            });
-        },
-        getAllDevices() {
-          console.log(authentication.getAccessToken());
-          let apiEndpoint1 = 'https://ivlapiadmin.azurewebsites.net/v1/devices';
-          let accessToken1 = `Bearer ${authentication.getAccessToken()}`;
-          this.axios.get(apiEndpoint1, {headers: {'authorization': accessToken1}})
-                  .then((response) => {
-                  this.allDevices = response.data;
-                  console.log('successfully retrieved all devices from azure sql: ');
-                  console.log(this.allDevices[0]);
-                  //console.log(response.data);
-                  }).catch(function (error) {
-                  console.log('error');
-                  console.log(error);
-          });
-        },
-        getAllGroups() {
-
-          let apiEndpoint2 = 'https://ivlapiadmin.azurewebsites.net/v1/groups';
-          let accessToken2 = `Bearer ${authentication.getAccessToken()}`;
-          this.axios.get(apiEndpoint2, {headers: {'authorization': accessToken2}})
-                  .then((response) => {                
-                  //populate firmware drop down list array
-                  var arrayLength = response.data.length;
-                  for (var i = 0; i < arrayLength; i++) {
-                      this.ddGroups.push({ text: response.data[i]['name'], value: response.data[i]['group_id'] });
-                  }
-                  console.log('successfully retrieved all groups from azure sql: ');
-                  console.log(response.data);
-                  }).catch(function (error) {
-                  console.log('error');
-                  console.log(error);
-          });
-
-        }
+export
+  default {
+    data() {
+      return {
+        inputDeviceId: '',
+        firmware: [],
+        allFirmware: [],
+        deviceUploadBodyMFOX: {},
+        allDevices: [],
+        selectedGroup: '',
+        ddGroups: [],
+        fields: [
+          { key: 'deviceid', label: 'Device ID', sortable: true, sortDirection: 'desc' },
+          { key: 'group', label: 'Group', sortable: true, class: 'text-center' },
+          { key: 'desired_fw', label: 'Desired FW Version', sortable: true, class: 'text-center' },
+          { key: 'reported_fw', label: 'Reported FW Version', sortable: true, class: 'text-center' },
+          { key: 'last_report_date', label: 'Last Report Date', sortable: true, class: 'text-center' }
+        ],
+      }
     },
-
-    /* view.js has a set of lifecycle hooks - created, mounted, updated, and destroyed */
-    created: function(){
+    methods: {
+      handleLoadModal() {
+        this.inputDeviceId = '';
+        this.selectedGroup = '';
+      },
+      handleOk(evt) {
+        evt.preventDefault()
+        if (!this.inputDeviceId) {
+            alert('Please enter a device id')
+        } else if (!this.selectedGroup) {
+            alert('Please select a group')
+        } else {
+            this.handleSubmit()
+        }
+      },
+      handleSubmit() {
+        this.deviceUploadBodyMFOX = {
+          deviceid: this.inputDeviceId,
+          group_id : this.selectedGroup
+          };
+        this.handleLoadModal()
+        this.$nextTick(() => {
+          this.$refs.modal.hide()
+        })
+        this.uploadToMFOX();
+      },
+      async loadTextFromFile(ev) {
+        this.firmwareFileContent = ev.target.files[0];
+        const file = ev.target.files[0];
+        this.firmwareImage = file.name;
+        var reader = new FileReader();
+        reader.onload = function(){
+        };
+        reader.readAsBinaryString(file);
+      },
+      uploadToMFOX() {
+        let apiEndpoint = 'https://ivlapiadmin.azurewebsites.net/v1/devices';
+        let accessToken = `Bearer ${authentication.getAccessToken()}`;
+        this.axios.post(apiEndpoint, this.deviceUploadBodyMFOX, {headers: {'authorization': accessToken}})
+        .then( (response) => {
+          console.log(`response: ${response}`); // eslint-disable-line
+          this.getAllDevices();
+        }).catch( (error) => {
+          console.log(`error: ${error}`); // eslint-disable-line
+        });
+      },
+      getAllDevices() {
+        let apiEndpoint1 = 'https://ivlapiadmin.azurewebsites.net/v1/devices';
+        let accessToken1 = `Bearer ${authentication.getAccessToken()}`;
+        this.axios.get(apiEndpoint1, {headers: {'authorization': accessToken1}})
+          .then((response) => {
+            this.allDevices = response.data;
+          }).catch(function (error) {
+            console.log(`error: ${error}`); // eslint-disable-line
+        });
+      },
+      getAllGroups() {
+        let apiEndpoint2 = 'https://ivlapiadmin.azurewebsites.net/v1/groups';
+        let accessToken2 = `Bearer ${authentication.getAccessToken()}`;
+        this.axios.get(apiEndpoint2, {headers: {'authorization': accessToken2}})
+        .then((response) => {                
+          //populate group drop down list array
+          var arrayLength = response.data.length;
+          for (var i = 0; i < arrayLength; i++) {
+              this.ddGroups.push({ text: response.data[i]['name'], value: response.data[i]['group_id'] });
+          }
+        }).catch(function (error) {
+          console.log(`error: ${error}`); // eslint-disable-line
+        });
+      }
+    },
+    created:
+      function(){
         this.getAllDevices();
         this.getAllGroups();
-
-
-    }
-  }
+      }
+  }
 </script>
