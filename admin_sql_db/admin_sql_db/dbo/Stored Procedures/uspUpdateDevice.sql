1ca5a1e6fb7ad25e7c8e3782a69b935d8c6cd2af
Erik Olsen <eriko@valencelevel.com>
2019-05-20 11:05:50 -0700

    Add transactions to t-sql device/group update stored procs

--- admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspUpdateDevice.sql
+++ admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspUpdateDevice.sql
@@ -2,25 +2,27 @@
   @deviceid bigint, @groupid nchar(100), @result INT OUTPUT
 AS
 BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			IF EXISTS(SELECT 1 FROM vwDevice WHERE DeviceId = @deviceid)
+			BEGIN
+			UPDATE Device
+			SET DeviceGroupId = @groupid 
+			WHERE
+			Id = @deviceid;
 
-if exists(Select 1 From vwDevice where DeviceId = @deviceid)
-BEGIN
-
-  UPDATE Device
-  Set DeviceGroupId = @groupid 
-  WHERE
-  Id = @deviceid;
-
-  IF @@rowcount = 1
-    SET @result = 1 --successful update
-  ELSE
-    SET @result = 4 --unsuccessful update 
-
-  SELECT * FROM vwDevice where DeviceId = @deviceid;
-
-END
-ELSE
-  SET @result = 2 --device not found
-  
-		
+			IF @@rowcount = 1
+				SET @result = 1 --successful update
+			ELSE
+				SET @result = 4 --unsuccessful update 
+			SELECT * FROM vwDevice where DeviceId = @deviceid;
+			END
+			ELSE
+				SET @result = 2 --device not found
+			COMMIT
+	END TRY
+	BEGIN CATCH
+		SET @result = 4 --unsuccessful update
+		ROLLBACK
+	END CATCH	
 END
--- admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspUpdateGroup.sql
+++ admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspUpdateGroup.sql
@@ -2,46 +2,45 @@
   @groupid int, @name nchar(100), @desiredfwid nchar(100), @result INT OUTPUT
 AS
 BEGIN
-  
-if exists(Select 1 From vwGroup where GroupId = @groupid)
-BEGIN
+	BEGIN TRY
+		BEGIN TRANSACTION
+			IF EXISTS(SELECT 1 FROM vwGroup WHERE GroupId = @groupid)
+			BEGIN
+				IF @desiredfwid IS NOT NULL
+				BEGIN
+					UPDATE DeviceGroup
+					Set 
+					Name = @name,
+					DesiredFirmwareId = @desiredfwid
+					WHERE
+					Id = @groupid;
 
-  IF @desiredfwid IS NOT NULL
-  BEGIN
-	  UPDATE DeviceGroup
-	  Set 
-		Name = @name,
-		DesiredFirmwareId = @desiredfwid
-	  WHERE
-		Id = @groupid;
+					IF @@rowcount = 1
+					SET @result = 1 --successful update
+					ELSE
+					SET @result = 4 --unsuccessful update 
+				END
+				ELSE
+				BEGIN
+  					UPDATE DeviceGroup
+					Set 
+					Name = @name
+					WHERE
+					Id = @groupid;
 
-	  IF @@rowcount = 1
-	    SET @result = 1 --successful update
-	  ELSE
-	    SET @result = 4 --unsuccessful update 
-
-  END
-  ELSE
-  BEGIN
-  	  UPDATE DeviceGroup
-	  Set 
-		Name = @name
-	  WHERE
-		Id = @groupid;
-
-	  IF @@rowcount = 1
-	    SET @result = 1 --successful update
-	  ELSE
-	    SET @result = 4 --unsuccessful update 
-
-  END
-
-  SELECT * FROM vwGroup where GroupId = @groupid;
-
-END
-ELSE
-  SET @result = 2 --group not found
-
-
-		
+					IF @@rowcount = 1
+					SET @result = 1 --successful update
+					ELSE
+					SET @result = 4 --unsuccessful update 
+				END
+				SELECT * FROM vwGroup where GroupId = @groupid;
+			END
+			ELSE
+				SET @result = 2 --group not found
+			COMMIT
+	END TRY
+	BEGIN CATCH
+		SET @result = 4 --unsuccessful update
+		ROLLBACK
+	END CATCH
 END
