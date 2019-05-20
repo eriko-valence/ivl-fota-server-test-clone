f1954dac84f5b85f5eac55514c45e67c2958ff97
Erik Olsen <eriko@valencelevel.com>
2019-05-20 11:59:36 -0700

    Add transactions to t-sql device/group delete stored procs

--- admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspDeleteFirmware.sql
+++ admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspDeleteFirmware.sql
@@ -7,24 +7,30 @@ CREATE PROCEDURE [dbo].[uspDeleteFirmware]
   @firmwareid int, @result INT OUTPUT
 AS
 BEGIN
- 
- DECLARE @groupcount INT, @fwexists INT
+	BEGIN TRY
+		BEGIN TRANSACTION
+			DECLARE @groupcount INT, @fwexists INT
+			--Only firmware not attached to device groups can be deleted.
+			SET @groupcount = (SELECT count(*) FROM DeviceGroup WHERE DesiredFirmwareId = @firmwareid);
 
---Only firmware not attached to device groups can be deleted.
-SET @groupcount = (select count(*) from DeviceGroup where DesiredFirmwareId = @firmwareid);
+			SET @fwexists = (SELECT count(*) FROM DeviceFirmware WHERE Id = @firmwareid);
 
-SET @fwexists = (select count(*) from DeviceFirmware where Id = @firmwareid);
-
-IF @fwexists = 0
-    SET @result = 2; --Firmware does not exist
-ELSE IF @groupcount > 0
-	SET @result = 3; --Only firmware not currently assigned to any groups can be deleted.
-ELSE
-	BEGIN
-	delete from DeviceFirmware where Id = @firmwareid;
-		if @@rowcount = 1
-		  SET @result = 1 --successful deletion
-		else
-		  SET @result = 4 --firmware not deleted 
-	END
+			IF @fwexists = 0
+				SET @result = 2; --Firmware does not exist
+			ELSE IF @groupcount > 0
+				SET @result = 3; --Only firmware not currently assigned to any groups can be deleted.
+			ELSE
+				BEGIN
+				DELETE FROM DeviceFirmware WHERE Id = @firmwareid;
+					IF @@rowcount = 1
+					  SET @result = 1 --successful deletion
+					else
+					  SET @result = 4 --firmware not deleted 
+				END
+			COMMIT
+	END TRY
+	BEGIN CATCH
+		SET @result = 4 --unsuccessful update
+		ROLLBACK
+	END CATCH
 END
--- admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspDeleteGroup.sql
+++ admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspDeleteGroup.sql
@@ -11,23 +11,27 @@ CREATE PROCEDURE [dbo].[uspDeleteGroup]
   @groupid int, @result INT OUTPUT
 AS
 BEGIN
- 
- DECLARE @devicecount INT, @groupexists INT
-
-SET @devicecount = (select count(*) from Device where DeviceGroupId = @groupid);
-
-SET @groupexists = (select count(*) from DeviceGroup where Id = @groupid);
-
-IF @groupexists = 0
-    SET @result = 2; --Group does not exist
-ELSE IF @devicecount > 0
-	SET @result = 3; --Only groups not currently assigned to any devices can be deleted.
-ELSE
-	BEGIN
-	delete from DeviceGroup where Id = @groupid;
-		if @@rowcount = 1
-		  SET @result = 1 --successful deletion
-		else
-		  SET @result = 4 --group not deleted 
-	END
+	BEGIN TRY
+		BEGIN TRANSACTION
+			DECLARE @devicecount INT, @groupexists INT
+			SET @devicecount = (select count(*) from Device where DeviceGroupId = @groupid);
+			SET @groupexists = (select count(*) from DeviceGroup where Id = @groupid);
+			IF @groupexists = 0
+				SET @result = 2; --Group does not exist
+			ELSE IF @devicecount > 0
+				SET @result = 3; --Only groups not currently assigned to any devices can be deleted.
+			ELSE
+				BEGIN
+				DELETE FROM DeviceGroup WHERE Id = @groupid;
+					if @@rowcount = 1
+						SET @result = 1 --successful deletion
+					else
+						SET @result = 4 --group not deleted 
+				END
+			COMMIT
+	END TRY
+	BEGIN CATCH
+		SET @result = 4 --unsuccessful update
+		ROLLBACK
+	END CATCH
 END
