d148b6cb1fcc5802622e236139c2e80af10ae12c
Erik Olsen <eriko@valencelevel.com>
2019-06-04 22:11:46 -0700

    Update function app package creation script #24

--- infrastructure/createZipPackage.ps1
+++ infrastructure/createZipPackage.ps1
@@ -1,15 +1,8 @@
-
 param (
     [Parameter(Mandatory=$true)][string]$app_source_code_dir,
     [Parameter(Mandatory=$true)][string]$zip_file_name
  )
- 
-  Write-Host $app_source_code_dir
-  Write-Host $zip_file_name
- 
-
-#$YourDirToCompress="C:\Users\ErikOlsen\Documents\_github\repos\ivl\iv_fota_server\admin_api"
-#$ZipFileResult="C:\_tmp\functionapp.zip"
+  
 $dir_to_exclude=@(".vscode")
 
 Get-ChildItem $app_source_code_dir | where { $_.Name -notin $dir_to_exclude} | Compress-Archive -DestinationPath $zip_file_name -Update
