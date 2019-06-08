ca9ace666791345c950cf0cd6aa4e82313ba14ac
Erik Olsen <eriko@valencelevel.com>
2019-06-07 17:35:08 -0700

    Add bug fix to prevent duplicate firmware versions #46

--- admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspCreateFirmware.sql
+++ admin_sql_db/admin_sql_db/dbo/Stored Procedures/uspCreateFirmware.sql
@@ -2,20 +2,33 @@
   @version nchar(100), @signature nchar(1000), @md5 nchar(100), @blobname nchar(100), @blobcontainer nchar(100), @result INT OUTPUT
 AS
 BEGIN
-  
-  INSERT INTO DeviceFirmware([Version], [Signature], [Md5], [BlobName], [BlobContainer] ) 
-	VALUES(
-		@version,
-		@signature,
-		@md5,
-		@blobname,
-		@blobcontainer);
+	BEGIN TRY
+		BEGIN TRANSACTION
+			IF EXISTS(SELECT 1 FROM vwFirmware WHERE [Version] = @version)
+			  SET @result = 6 --firmware already exists in database
+			ELSE
+			BEGIN
+			   INSERT INTO DeviceFirmware([Version], [Signature], [Md5], [BlobName], [BlobContainer] ) 
+				VALUES(
+					@version,
+					@signature,
+					@md5,
+					@blobname,
+					@blobcontainer);
 
-  IF @@rowcount = 1
-    SET @result = 1 --successful creation
-  ELSE
-	SET @result = 4 --unsuccessful creation 
+			  IF @@rowcount = 1
+				SET @result = 1 --successful creation
+			  ELSE
+				SET @result = 4 --unsuccessful creation 
 
-  SELECT * FROM vwFirmware where BlobName = @blobname;
-		
+			  SELECT * FROM vwFirmware where BlobName = @blobname;
+			END
+		COMMIT
+	END TRY
+	BEGIN CATCH
+		SET @result = 4 --unsuccessful creation
+		ROLLBACK
+	END CATCH	
+
+
 END
