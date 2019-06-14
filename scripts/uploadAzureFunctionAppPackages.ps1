18d8e90dea3df28101de188f5164d8e4e549b33b
Erik Olsen <eriko@valencelevel.com>
2019-06-14 14:37:18 -0700

    Update azure function app build and deploy script

--- scripts/uploadAzureFunctionAppPackages.ps1
+++ scripts/uploadAzureFunctionAppPackages.ps1
@@ -11,32 +11,33 @@ $sas_expiration = "2030-05-31"
 # files/folders to exclude from package files
 $dir_to_exclude=@(".vscode")
 
-#package zip azure storage container name
+# package zip azure storage container name
 $function_release_container = "function-releases";
 
-#source code directories
+# source code directories
 $source_code_dir_admin_api  = "..\admin_api";
 $source_code_dir_device_api = "..\device_api";
 
-#package zip file names
+# package zip file names
 $package_file_name_admin_api = "functionapp-admin-api.zip";
 $package_file_name_device_api = "functionapp-device-api.zip";
 
-#package zip directory
-$package_zip_directory = ".\functionapp-releases\";
+# package zip directory
+$package_zip_directory = "..\infrastructure\functionapp-releases\";
 
-#package zip file paths
+# package zip file paths
 $package_zip_file_path_admin_api = "$($package_zip_directory)\$($package_file_name_admin_api)"
 $package_zip_file_path_device_api = "$($package_zip_directory)\$($package_file_name_device_api)"
 
-#create package zip files
+# create package zip files
 Get-ChildItem $source_code_dir_admin_api | where { $_.Name -notin $dir_to_exclude} | Compress-Archive -DestinationPath $package_zip_file_path_admin_api -Update
 Get-ChildItem $source_code_dir_device_api | where { $_.Name -notin $dir_to_exclude} | Compress-Archive -DestinationPath $package_zip_file_path_device_api -Update
 
-#upload package zip files to azure storage
+# upload package zip files to azure storage
 az storage blob upload --account-name $azure_storage_account_name -f $package_zip_file_path_admin_api -c $function_release_container -n $package_file_name_admin_api 2> $null
 az storage blob upload --account-name $azure_storage_account_name -f $package_zip_file_path_device_api -c $function_release_container -n $package_file_name_device_api 2> $null
 
+# generate azure storage blob sas uri
 $blob_sas_full_uri_admin_api = $(az storage blob generate-sas --account-name $azure_storage_account_name --container-name $function_release_container --name $package_file_name_admin_api --permissions r --expiry $sas_expiration --full-uri)
 $blob_sas_full_uri_device_api = $(az storage blob generate-sas --account-name $azure_storage_account_name --container-name $function_release_container --name $package_file_name_device_api --permissions r --expiry $sas_expiration --full-uri)
 
